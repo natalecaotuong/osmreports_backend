7b537c24c2f22447fd31b6af2cda6a4eff269e43
natalecaotuong <82642047+natalecaotuong@users.noreply.github.com>
2021-06-08 23:33:37 +0200

    Created 2 tables

--- createdb.js
+++ createdb.js
@@ -3,9 +3,22 @@ var sqlite = require('sqlite3');
 var db = new sqlite.Database("users.sqlite3");
 
 db.run(`CREATE TABLE users(
-  id INTEGER PRIMARY KEY,
+  user_id INTEGER PRIMARY KEY,
   username TEXT NOT NULL,
   password TEXT NOT NULL
+)`);
+
+db.run(`CREATE TABLE user_report(
+  report_id INTEGER FOREIGN KEY,
+  user_id INTEGER FOREIGN KEY,
+  duration DATE NOT NULL,
+  vote INTEGER NOT NULL,
+  description STRING NOT NULL,
+)`);
+
+db.run(`CREATE TABLE report(
+  report_id INTEGER PRIMARY KEY,
+  type STRING NOT NULL
 )`);
 
 db.close();
